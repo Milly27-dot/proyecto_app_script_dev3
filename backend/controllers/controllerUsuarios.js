a5c093fb6eec601b2a0d77211d7c6edec95c12cb
Milly-dot <l21070072@valladolid.tecnm.mx>
2025-02-27 22:55:56 -0600

    Video 14

--- backend/controllers/controllerUsuarios.js
+++ backend/controllers/controllerUsuarios.js
@@ -8,18 +8,36 @@ function guardarUsuario(usuario) {
         return {
             titulo: "Registro Correcto",
             descripcion: "Ya se encuentra el usuario en la base de datos.",
-        }
+        }    
     } catch (error) {
         return {
-            titulo: "Una disculpa, pero ha sucedido un error en la solicitud " + error,
+            titulo: "Una disculpa, pero ha sucedido un error en la solicitud" + error,  
             descripcion: "Por favor contacte a soporte.",
         }
     }
-
+    
 }
 
-function listarUsuarios(id = undefined) {
+function listarUsuarios(id = undefined){
     //return obtenerDatos(env_().SH_REGISTRO_USUARIOS);
-
     return JSON.stringify(_read(obtenerSheet(env_().SH_REGISTRO_USUARIOS), id));
+}
+
+function actualizarUsuario(id, datos) {
+    
+    try {
+        const sheetUsuarios = obtenerSheet(env_().SH_REGISTRO_USUARIOS);
+        Update(id,datos, sheetUsuarios);
+        return {
+            titulo: "Actualizacion Realizada",
+            descripcion: "El Usuario en la base de datos ha sido actualizado.",
+        }    
+    } catch (error) {
+        return {
+            titulo: "Una disculpa, pero ha sucedido un error en la solicitud " + error,  
+            descripcion: "Por favor contacte a soporte.",
+        }
+    }
+    
+    
 }
